84b1c539fc5e96fe7b2d9abc55c191c8a670022b
sh <shwetashinde900@gmail.com>
2024-04-08 01:38:45 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+function App() {
+  const inlineStyles = {
+    width: '100vh',
+    height: '500px',
+    border: '1px solid black'
+    // Add any other styles you need here
+  };
+
+  return (
+    <>
+      <div style={inlineStyles}>
+      <input>
+      
+      </input> 
+      <input type='submit' value={"Send"}>
+
+      </input>
+      </div>
+    </>
+  )
+}
+
+export default App
